f18e09f6b727f6063f024aea25bfeee23630f0b8
Mx. Kas Perch <the@nodebotani.st>
2018-04-12 22:58:18 -0500

    MQTT connect logs

--- rpi/hub/index.js
+++ rpi/hub/index.js
@@ -1,7 +1,17 @@
 const mosca = require('mosca')
 
 const server = new mosca.Server({
+  interfaces: [
+    {
+      type: 'ws',
+      port: 1337
+    }
+  ],
   port: 1883,
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: "mongodb://localhost:27017/mosca"
+  },
   backend: {
     type: 'mongo',
     url: 'mongodb://localhost:27017/mqtt',
@@ -13,3 +23,8 @@ const server = new mosca.Server({
 server.on('ready', () => {
   console.log('MQTT Broker up ad running!')
 })
+
+
+server.on('clientConnected', function(client) {
+	console.log('client connected', client.id);		
+});
