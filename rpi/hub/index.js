2b24a881c37c75dfff6aa529277bf5bf84cc17a7
Mx. Kas Perch <the@nodebotani.st>
2018-04-15 20:26:14 -0500

    Fixed some MQTT broker issues

--- rpi/hub/index.js
+++ rpi/hub/index.js
@@ -1,5 +1,3 @@
-import { getPackedSettings } from 'http2';
-
 const mosca = require('mosca')
 
 const server = new mosca.Server({
@@ -17,12 +15,13 @@ server.on('ready', () => {
 })
 
 server.on('published', (packet, client) => {
-  console.log(`Message: ${packet}\nClient:${client}\n\n`)
+  client = client || { id: 'N/A' }
+  console.log(`Topic: ${packet.topic}\nMessage: ${packet.payload.toString()}\nClient:${client.id}\n\n`)
 })
 
 server.on('clientConnected', function(client) {
   console.log('client connected', client.id);
-  setInterval(() => {
-    server.publish('Hello', 'From the Raspi!')
-  }, 5000)
+  // setInterval(() => {
+  //   server.publish('Hello', 'From the Raspi!')
+  // }, 5000)
 });
