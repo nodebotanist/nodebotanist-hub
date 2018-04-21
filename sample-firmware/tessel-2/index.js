c43f94321ccc72d4f8bdd9f1332f77d9129a8d92
Mx. Kas Perch <the@nodebotani.st>
2018-04-21 14:25:18 -0500

    Firmware ready

--- sample-firmware/tessel-2/index.js
+++ sample-firmware/tessel-2/index.js
@@ -1,11 +1,21 @@
 const tessel = require('tessel')
 const j5 = require('johnny-five')
 const tesselIO = require('tessel-io')
+const mqtt = require('mqtt')
 
 let board = j5.Board({
   io: tesselIO
 })
 
 board.on('ready', () => {
-  
+  let client = mqtt.connect('nodebotanist-hub.local')
+
+  client.on('connect', () => {
+    client.subscribe('Hello')
+    console.log('MQTT client connected to broker')
+  })
+
+  client.on('message', (topic, payload) => {
+    console.log(topic + ': ' + payload.toString())
+  })
 })
