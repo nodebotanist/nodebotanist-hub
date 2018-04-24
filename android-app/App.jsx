f1575ee2304604d9a9e57d5514649385bddbcf44
Mx. Kas Perch <the@nodebotani.st>
2018-04-24 10:57:41 -0500

    Prettier

--- android-app/App.jsx
+++ android-app/App.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
+import { Platform, StyleSheet, Switch } from 'react-native';
 import {
-  Platform,
-  StyleSheet,
-  Switch
-} from 'react-native'
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-import mqtt from 'rn-mqtt'
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
+import mqtt from 'rn-mqtt';
 
 const myStorage = {
   setItem: (key, item) => {
     myStorage[key] = item;
   },
-  getItem: (key) => myStorage[key],
-  removeItem: (key) => {
+  getItem: key => myStorage[key],
+  removeItem: key => {
     delete myStorage[key];
-  },
+  }
 };
-
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -34,39 +42,43 @@ export default class App extends Component<Props> {
           </Body>
         </Header>
         <Content>
-          <Button full success onPress={(value)=> {this.toggleLED(value)}}>
+          <Button
+            full
+            success
+            onPress={value => {
+              this.toggleLED(value);
+            }}
+          >
             <Text>Toggle LED</Text>
           </Button>
         </Content>
-        <Footer>
-
-        </Footer>
+        <Footer />
       </Container>
     );
-  }  
+  }
   componentDidMount() {
     const client = new mqtt.connect('mqtt://192.168.1.107:1883');
-    
-    client.on('connectionLost', (responseObject) => {
+
+    client.on('connectionLost', responseObject => {
       if (responseObject.errorCode !== 0) {
         console.log(responseObject.errorMessage);
       }
     });
 
-    client.on('message', (message) => {
+    client.on('message', message => {
       console.log(message.payloadString);
     });
-    
+
     // connect the client
     client.on('connect', () => {
-        client.subscribe('Hello')
-        setInterval(() => {
-          client.publish('Hello', 'from the note!');
-        }, 5000)
-    })
+      client.subscribe('Hello');
+      setInterval(() => {
+        client.publish('Hello', 'from the note!');
+      }, 5000);
+    });
   }
   toggleLED(value) {
-    console.log(value)
+    console.log(value);
   }
 }
 
